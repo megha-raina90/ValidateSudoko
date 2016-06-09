25d5cc1f6685986382acac196371e54506ae0732
megha-raina90 <megharaina33@gmail.com>
2016-06-09 19:07:05 +0530

    This code validates sudoku puzzle sol

---
+++ src/SudukoValidation.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class SudukoValidation {
+
+	public boolean checkSudokuStatus(char[][] grid) {
+
+		for (int i = 0; i < 9; i++) {
+
+			char[] row = new char[9];
+			char[] square = new char[9];
+			char[] column = grid[i].clone();
+
+			for (int j = 0; j < 9; j++) {
+				row[j] = grid[j][i];
+				square[j] = grid[(i / 3) * 3 + j / 3][i * 3 % 9 + j % 3];
+			}
+			if (!(validate(column) && validate(row) && validate(square)))
+				return false;
+		}
+		return true;
+	}
+
+	public boolean validate(char[] column) {
+		int i = 0;
+		Arrays.sort(column);
+		
+		for (char number : column) {
+			
+			int temp = Character.getNumericValue(number);
+			if (temp != ++i)
+				return false;
+		}
+		return true;
+	}
+
+
+
+}
